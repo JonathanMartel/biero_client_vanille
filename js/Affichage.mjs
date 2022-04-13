7c4633b542c38fe4e9b58be2f80c59edb9d32f36
Jonathan Martel <jonathmartel@gmail.com>
2022-04-13 19:15:09 -0400

    retrait de affichage.mjs

--- js/Affichage.mjs
+++ js/Affichage.mjs
@@ -6,43 +6,4 @@
  */
 export default class Affichage {
 
-
-    static afficheEnregistrer(){
-        const template = document.querySelector("#tmplEnregistrer").innerHTML;
-        const render = Mustache.render(template, {});
-        document.querySelector("main").innerHTML = render;
-    }
-    /**
-     * Convertie un template Mustache.js en Element et l'insère dans un noeud.
-     *
-     * @static
-     * @param {String} tmpl
-     * @param {Object} data
-     * @param {Element} noeud
-     * @memberof Affichage
-     */
-    static afficherTemplate(tmpl, data, noeud){
-        
-        noeud.innerHTML = Mustache.render(tmpl, data);
-    }
-    /**
-     * Permet le chargement asynchrone des templates pour Mustache.js
-     * 
-     * @static
-     * @param {{fichier: String, data: Object, tmpl:String}[]} aTemplates - Tableau qui contient la liste des fichiers à charger
-     * @returns ?
-     * @memberof Affichage
-     */
-    static chargementTemplate(aTemplates){
-        let htmlTemplate = [];
-        aTemplates.forEach(uneRoute=>{
-            console.log(uneRoute.fichier);
-            htmlTemplate.push(fetch("./vues/"+uneRoute.fichier)
-                .then(reponse => reponse.text())
-                    .then(template => uneRoute.tmpl = template));
-                //.then(function(reponse) {return reponse.text()});
-        })
-
-        return Promise.all(htmlTemplate);
-    }
 }

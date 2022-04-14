7b07fef0e549d56ce8584b32de6e1a76007ae5af
Jonathan Martel <jonathmartel@gmail.com>
2022-04-13 22:05:40 -0400

    fin du cours

--- js/Affichage.mjs
+++ js/Affichage.mjs
@@ -5,5 +5,26 @@
  * @requires Mustache.js {https://unpkg.com/mustache@latest}
  */
 export default class Affichage {
+    static chargement = {};
+
+    static chargementTemplate(nomTemplate, dom_parent, data){
+        if(this.chargement[nomTemplate])
+        {
+            let vue = Mustache.render(this.chargement[nomTemplate], data);
+            dom_parent.innerHTML = vue;
+        }
+        else{
+            fetch("./vues/"+nomTemplate + ".html")
+                .then((reponse)=> reponse.text())
+                .then((template)=>{
+                    this.chargement[nomTemplate] = template;
+                    let vue = Mustache.render(template, data);
+                    dom_parent.innerHTML = vue;
+                    console.log(this.chargement);
+                })
+        }
+        
+
+    }
 
 }
--- js/main.mjs
+++ js/main.mjs
@@ -4,36 +4,80 @@ import page from "//unpkg.com/page/page.mjs";
 
 // IIFE (Immediatly invoqued function expression)
 (function(){
-    
-    page("/", ()=>{
-        console.log("accueil");
+    let _app = document.querySelector(".app");
+
+    let _route = [
+        {
+            route : "/", 
+            cb : cbAccueil
+        },
+        {
+            route : "/liste", 
+            cb : [cbLog, cbListe]
+        },
+        {
+            route : "/contact", 
+            cb : cbContact
+        },
+    ]
+    _route.forEach((route)=>{
+        if(Array.isArray(route.cb)){
+            page(route.route, ...route.cb);        
+        }
+        else{
+            page(route.route, route.cb);
+        }
+        
+        
     })
-    page("/liste", ()=>{
+   
+    /*page("/", cbAccueil)
+    page("/liste", cbListe)
+    page("/contact", cbContact)*/
+
+     // Toujours s'assurer que le DOM est prêt avant de manipuler le HTML.
+    document.addEventListener("DOMContentLoaded", ()=>{
+        Affichage.chargementTemplate("nav", document.querySelector(".nav"), {});
+        
+        page({
+            hashbang:true
+        });   
+    
+       
+        
+        
+
+    })
+
+
+    function cbAccueil(){
+        console.log("accueil");
+        Affichage.chargementTemplate("accueil", _app, {});
+    }
+
+    function cbListe(){
         console.log("liste de bière");
         Biere.getListeBieres().then((listeBiere)=>{
             console.log(listeBiere); 
-            let DOMListe = document.querySelector(".liste"); 
-            let chaineDOM;
-            chaineDOM = "<ul>";
-            listeBiere.data.forEach(element => {
-                chaineDOM += ("<li>" + element.nom +"</li>");
-            });
-            chaineDOM += "</ul>";
-            DOMListe.innerHTML = chaineDOM;
-
+            
+            Affichage.chargementTemplate("liste", _app, listeBiere);
 
         });
-    })
-    
-    page({
-        hashbang:true
-    });   
+    }
 
-    // Toujours s'assurer que le DOM est prêt avant de manipuler le HTML.
-    document.addEventListener("DOMContentLoaded", ()=>{
-        //console.log("Prêt");
-        
-        
+    function cbContact(){
+        Affichage.chargementTemplate("contact", _app, {});
+    }
 
-    })
+    function cbLog(ctx, next){
+        console.log(ctx, next);
+        let log = document.querySelector("[name='log']");
+        console.log(log);
+        if(log.checked){
+            next();
+        }
+        else{
+            console.log("erreur");
+        }
+    }
 })()
